ce00ca4bf65ece15d5253dd582efab29c1fcd237
Said Maadan <saidmaadan@gmail.com>
2014-04-30 19:26:56 -0500

    Modified main file

--- main.js
+++ main.js
@@ -6,8 +6,8 @@ var spaces = [
   NaN, NaN, NaN
 ];
 
-var player1 = 'veggies';
-var player2 = 'junkfood';
+var player1 = 'Veggies';
+var player2 = 'Junkfood';
 var currentPlayer = null;
 
 var setNextTurn = function () {
@@ -20,6 +20,16 @@ var setNextTurn = function () {
   $('#turn-label').text(currentPlayer);
 };
 
+var tie = function() {
+  for (var b = 0; b < spaces.length; b++) {
+    if (!spaces[b]) {
+      return false;
+    }
+  }
+    return true;
+}
+
+
 var checkForWinner = function () {
   // Because (NaN === NaN) is always false, we can safely assume
   // that if three spaces in a row are the same, all three spaces are
@@ -28,32 +38,70 @@ var checkForWinner = function () {
   if ( spaces[0] === spaces[1] && spaces[1] === spaces[2]
     || spaces[3] === spaces[4] && spaces[4] === spaces[5]
     || spaces[6] === spaces[7] && spaces[7] === spaces[8]
-    // TODO: Check for rest of game winning cases
+    || spaces[0] === spaces[3] && spaces[3] === spaces[6]
+    || spaces[1] === spaces[4] && spaces[4] === spaces[7]
+    || spaces[2] === spaces[5] && spaces[5] === spaces[8]
+    || spaces[0] === spaces[4] && spaces[4] === spaces[8]
+    || spaces[2] === spaces[4] && spaces[4] === spaces[6]
   )
   {
     console.log('somebody won');
     // TODO: Trigger 'game-win' event with the winning player as the event data
+    $(document).trigger('game-win', currentPlayer);
+  }
+  else if (tie()) {
+    alert("The game is tie!");
+
+    $('#board .space').removeClass(player1);
+    $('#board .space').removeClass(player2);
+    spaces = [
+      NaN, NaN, NaN,
+      NaN, NaN, NaN,
+      NaN, NaN, NaN,
+    ];
+    console.log("The game is tie!")
+    setNextTurn();
   }
 };
 
-$(document).on('click', '#board .space', function (e) {
-  var spaceNum = $(e.currentTarget).index();
-  console.log('You clicked on space #' + spaceNum);
+$(document).on('click', '#board .space', function (event) {
+  event.preventDefault
+  //check if the current space has any classes other than "space"
+  // check if current space has more than 1 class
+  var spaceNum = $(event.currentTarget).index();
 
-  // Marks the space with the current player's name
-  // TODO: Don't mark it unless the space is blank
-  spaces[spaceNum] = currentPlayer;
-  // Adds a class to elem so css can take care of the visuals
-  $('#board .space:eq(' + spaceNum + ')').addClass(currentPlayer);
+  if ($('#board .space')[spaceNum].classList.length > 1) {
+    alert("Space has been taken, pick again.");
+  } else {
 
-  checkForWinner();
-  setNextTurn();
+    console.log('You clicked on space #' + spaceNum);
+
+    // Marks the space with the current player's name
+    // TODO: Don't mark it unless the space is blank
+    spaces[spaceNum] = currentPlayer;
+    // Adds a class to elem so css can take care of the visuals
+    $('#board .space:eq(' + spaceNum + ')').addClass(currentPlayer);
+
+    checkForWinner();
+    setNextTurn();
+  }
 });
+
 
 $(document).on('game-win', function (e, winner) {
   // TODO: Alert who won the game
-  alert("Congratulation! You won the game")
+  alert(winner + "! Congratulation.. You won this game")
+  $('#board .space').removeClass(player1);
+  $('#board .space').removeClass(player2);
+  spaces = [
+  NaN, NaN, NaN,
+  NaN, NaN, NaN,
+  NaN, NaN, NaN
+  ];
+  setNextTurn();
+
 });
 
 // Start the game
 setNextTurn();
+
